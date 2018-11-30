3534c231b46c80ec0dee4f04e411e42c39c18f7b
Bowen <tbow1382@740s-pl-120.local>
2018-11-30 08:45:30 -0700

    created BucketItemCell.swift

---
+++ DataApp/Model/BucketItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  BucketItemCell.swift
+//  DataApp
+//
+//  Created by Bowen, Tanner on 11/30/18.
+//  Copyright © 2018 CSP. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UICollectionViewCell {
+    
+}
